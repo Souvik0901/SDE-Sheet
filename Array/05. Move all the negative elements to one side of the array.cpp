97941591bea262ad1f0ba8b9c8f70ee503c14c39
Souvik Sen <88578056+hr-souvik@users.noreply.github.com>
2022-08-09 16:32:02 +0530

    Create 05. Move all the negative elements to one side of the array.cpp

---
+++ Array/05. Move all the negative elements to one side of the array.cpp
@@ -0,0 +1,31 @@
+
+
+class Solution{
+    public:
+    void segregateElements(int arr[],int n)
+    {
+        // Your code goes here
+        vector<int>nums;
+        int j=0;
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i]<0)
+            {
+                nums.push_back(arr[i]);
+            }
+            else
+            {
+                 arr[j++]=arr[i];
+            }
+        }
+        
+        
+        int neg=0;
+        for(int k=j;k<n; k++)
+        {
+            arr[k]=nums[neg++];
+        }
+           
+    }
+};
+

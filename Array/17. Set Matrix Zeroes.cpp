d89831ce0a3280eece5b2da5347803e9f5eec15e
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2023-01-11 18:21:26 +0530

    Update 17. Set Matrix Zeroes.cpp

--- Array/17. Set Matrix Zeroes.cpp
+++ Array/17. Set Matrix Zeroes.cpp
@@ -37,3 +37,65 @@ public:
         }
     }
 };
+
+Optimal Approach:
+
+Time Complexity: 0(MN)
+ Space Complexity: 0(1)
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();     // row size
+        int m = matrix[0].size();   // column size
+        bool isRow0=false;
+        bool isCol0= false;
+
+        for(int i=0; i<n; i++)
+        {
+            if(matrix[i][0]==0)
+            isRow0 =true;
+        }
+
+        for(int j=0; j<m; j++)
+        {
+            if(matrix[0][j]==0)
+            isCol0 =true;
+        }
+
+        for(int i=1; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        
+        }
+
+        for(int i=1; i<n; i++)
+        {
+            for(int j =1; j<m; j++)
+            {
+                if(matrix[i][0]==0 or matrix[0][j]==0)
+                matrix[i][j]=0;
+
+            }
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            if(isRow0==true)
+            matrix[i][0] =0;
+        }
+
+         for(int j=0; j<m; j++)
+        {
+            if(isCol0==true)
+            matrix[0][j] =0;
+        }
+    }
+};

edb9f12b7db1efcaa34766d30f3fd396974a105d
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2025-08-03 21:56:04 +0530

    Update 00LinkedList01.cpp

--- LinkedList/00LinkedList01.cpp
+++ LinkedList/00LinkedList01.cpp
@@ -80,7 +80,7 @@ Node* mergeTwoList(Node* l1, Node* l2){
         l1->next = mergeTwoList(l1->next, l2);
         return l1;
     }
-    else if(l1->data> l2->data){
+    else {
         l2->next = mergeTwoList(l1, l2->next);
         return l2;
     }

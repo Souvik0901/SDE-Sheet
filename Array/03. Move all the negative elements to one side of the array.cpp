32e6dae1933687939fe622461a18ff07ba7dfd5b
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2025-08-14 13:46:41 +0530

    Update 03. Move all the negative elements to one side of the array.cpp

--- Array/03. Move all the negative elements to one side of the array.cpp
+++ Array/03. Move all the negative elements to one side of the array.cpp
@@ -30,6 +30,32 @@ class Solution{
 };
 
 
+---------------------------------------------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   vector<int>nums= {4,5,6,-2,-1,7,-3,-8,1,3,5,-1};
+   int k = nums.size()-1;
+   int i =0;
+   while(i<=k)
+  {
+        if(nums[i]<0 && nums[k]>0)  swap(nums[i++], nums[k--]);
+        else if(nums[i]<0 && nums[k]<0) k--;
+        else {
+            i++;   // important fix
+        }
+   }
+   for(int i=0; i<nums.size(); i++){
+       cout<<nums[i]<< ",";
+   }
+}
+
+//Time Complexity: 0(N)
+//sapce Complexity: 0(1)
+--------------------------------------------------------------------------------
+
 
 
 

76d2314a6838bbf97908db163f264b90c83f02a2
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2025-08-18 01:21:03 +0530

    Create 04.Unique Path.cpp

---
+++ Dynamic Programming/04.Unique Path.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int total = m+n-2;
+        int right = m-1;
+        double ans=1;
+        
+        for(int i=1; i<=right; i++)
+        {
+            ans = ans*(total-right+i)/i;
+        }
+        return (int) ans;
+    }
+};

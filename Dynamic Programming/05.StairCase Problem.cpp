89def3b91d9915ec8635159ac766dccc31061d08
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2025-08-21 12:11:32 +0530

    Create 05.StairCase Problem.cpp

---
+++ Dynamic Programming/05.StairCase Problem.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countWays(int n) {
+    vector<int> dp(n+1, 0);
+
+    // Base cases
+    dp[0] = 1;  // 1 way to stay at ground
+    if(n >= 1) dp[1] = 1;  
+    if(n >= 2) dp[2] = 2;  
+
+    // Fill dp table
+    for(int i = 3; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+    }
+
+    return dp[n];
+}
+
+int main() {
+    int n;
+    cout << "Enter number of steps: ";
+    cin >> n;
+    cout << "Number of ways: " << countWays(n) << endl;
+    return 0;
+}
+
+
+
+//time complexity: 0(N)
+//Space Complexity: 0(N)

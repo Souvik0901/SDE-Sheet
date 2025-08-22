df1659bc85aa62ca29fca1c78b88cb8a1e90fc88
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2025-08-22 11:35:28 +0530

    Update 04. Remove Nth Node From Back of LL.cpp

--- LinkedList/04. Remove Nth Node From Back of LL.cpp
+++ LinkedList/04. Remove Nth Node From Back of LL.cpp
@@ -58,3 +58,29 @@ public:
         return dummy->next; // Return new head
     }
 };
+
+_________________________________________________________________________________
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+        ListNode* start = new ListNode(); // taking an empty listnode
+        start->next =  head;     //make it's next to head pointer
+
+        ListNode* fast= start;
+        ListNode* slow = start;
+
+        while(n--)fast= fast->next;
+
+        while(fast->next!=NULL){
+            fast=fast->next;
+            slow= slow->next;
+        }
+        slow->next= slow->next->next;
+
+        return start->next;
+    }
+
+};
+View

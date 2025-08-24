c682d5da56975ca2419f3ea27d12ebd17d98de3d
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2025-08-25 01:52:28 +0530

    https://leetcode.com/problems/unique-paths/

--- Dynamic Programming/04.Unique Path.cpp
+++ Dynamic Programming/04.Unique Path.cpp
@@ -12,3 +12,25 @@ public:
         return (int) ans;
     }
 };
+
+
+
+_____________________________________________________________________
+
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 1));
+        
+        // Fill the dp table
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        
+        return dp[m-1][n-1];
+    
+    }
+ //Time Complexity: 0(M*N)
+ //Space Complexity: 0(N)
+
+________________________________________________________________________

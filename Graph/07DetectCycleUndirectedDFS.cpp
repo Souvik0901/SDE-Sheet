499fc4743c78c2869bbedfbbd4aec44186fdfec6
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2025-09-01 13:27:09 +0530

    Update 07. Cycle Detection Using DFS.cpp

--- Graph/07DetectCycleUndirectedDFS.cpp
+++ Graph/07DetectCycleUndirectedDFS.cpp
@@ -43,7 +43,6 @@ int main()
         cin>>u>>v;
         adj[u].push_back(v);
         adj[v].push_back(u);
-
     }
 
     if(iscycle(adj,n)==1)
@@ -51,3 +50,51 @@ int main()
     else
        cout<<"Not a cycle";
 }
+
+_____________________________________________________________________________________________
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool dfs(int node, int parent, vector<int> adj[], vector<int>& vis) {
+    vis[node] = 1;
+
+    for(auto it : adj[node]) {
+        if(!vis[it]) {
+            if(dfs(it, node, adj, vis)) return true;
+        }
+        else if(it != parent) {
+            return true;  // cycle found
+        }
+    }
+    return false;
+}
+
+bool isCycle(int v, vector<int> adj[]) {
+    vector<int> vis(v+1, 0);
+    for(int i=1; i<=v; i++) {
+        if(!vis[i]) {
+            if(dfs(i, -1, adj, vis)) return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    int v = 5;
+    vector<int> adj[v+1];
+
+    // Graph with cycle
+    adj[1] = {2};
+    adj[2] = {1, 3, 5};
+    adj[3] = {2, 4};
+    adj[4] = {3, 5};
+    adj[5] = {4, 2};
+
+    if(isCycle(v, adj))
+        cout << "Cycle Detected\n";
+    else
+        cout << "Cycle Not Detected\n";
+}
+
+_________________________________________________________________________________________________
